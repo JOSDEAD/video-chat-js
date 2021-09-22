d0b5a161760d582339c6a86fb95a27c4634b74d9
JOSDEAD <jmmurillo61@gmail.com>
2021-09-22 12:40:55 -0600

    fixing the iceCandite having no media

--- src/helpers/joinRoom.js
+++ src/helpers/joinRoom.js
@@ -6,44 +6,45 @@ import { useDispatch } from "react-redux";
 import { addStreams } from "../state/actions/streamActions";
 import RTCPeerConnectionContext from "../context/rtcPeer";
 const JoinRoom = ({ children, ...rest }) => {
-const {firestore} = useContext(FirebaseContext);
+  const { firestore } = useContext(FirebaseContext);
   const pc = useContext(RTCPeerConnectionContext);
   const dispath = useDispatch();
   const remoteStream = new MediaStream();
   const getVideoStreams = async () => {
-    let localStream = navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    }).then((stream)=>{
-      getRemoteDescription(stream)
-dispath(addStreams(stream, remoteStream));
-    }
-      );
-
-    
+    let localStream = navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        getRemoteDescription(stream);
+        dispath(addStreams(stream, remoteStream));
+      });
   };
   getVideoStreams();
 
-  
-  var mediaConstraints = {'mandatory': {
-                      'OfferToReceiveAudio':true, 
-                      'OfferToReceiveVideo':true }};
-
-  const { callId } = { ...rest }.computedMatch.params;
-  const callDoc = firestore.collection("calls").doc(callId);
-  const answerCandidates = callDoc.collection("answerCandidates");
-  const offerCandidates = callDoc.collection('offerCandidates');
-  pc.onicecandidate = (event) => {
-    event.candidate && answerCandidates.add(event.candidate.toJSON());
-  };
   const getRemoteDescription = async (stream) => {
-          stream.getTracks().forEach(track => {
-                pc.addTrack(track,stream);
-            });
-            pc.addEventListener("track", async (event) => {
-              console.log('entro');
-            remoteStream.addTrack(event.track, remoteStream);
-          });
+    stream.getTracks().forEach((track) => {
+      pc.addTrack(track, stream);
+    });
+    pc.addEventListener("track", async (event) => {
+      console.log("entro");
+      remoteStream.addTrack(event.track, remoteStream);
+    });
+    var mediaConstraints = {
+      mandatory: {
+        OfferToReceiveAudio: true,
+        OfferToReceiveVideo: true,
+      },
+    };
+
+    const { callId } = { ...rest }.computedMatch.params;
+    const callDoc = firestore.collection("calls").doc(callId);
+    const answerCandidates = callDoc.collection("answerCandidates");
+    const offerCandidates = callDoc.collection("offerCandidates");
+    pc.onicecandidate = (event) => {
+      event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
     const callData = (await callDoc.get()).data();
     console.log(callData);
     const offerDescription = callData.offer;
@@ -54,22 +55,22 @@ dispath(addStreams(stream, remoteStream));
 
     const answer = {
       type: answerDescription.type,
-      sdp: answerDescription.sdp
+      sdp: answerDescription.sdp,
     };
 
-    await callDoc.update({answer});
+    await callDoc.update({ answer });
   };
 
-  offerCandidates.onSnapshot(snapshot => {
-    console.log('entro a offerCandidates join');
-    snapshot.docChanges().forEach(change => {
-      if(change.type=== 'added') {
+  offerCandidates.onSnapshot((snapshot) => {
+    console.log("entro a offerCandidates join");
+    snapshot.docChanges().forEach((change) => {
+      if (change.type === "added") {
         let data = change.doc.data();
-        console.log('added join candidate',data);
-        pc.addIceCandidate(new RTCIceCandidate(data))
+        console.log("added join candidate", data);
+        pc.addIceCandidate(new RTCIceCandidate(data));
       }
-    })
-  })
+    });
+  });
   return (
     <Route
       {...rest}
